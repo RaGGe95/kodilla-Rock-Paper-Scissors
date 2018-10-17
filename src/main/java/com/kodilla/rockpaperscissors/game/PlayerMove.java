e30a29076ae2fb8ea4452133ea2845e4790ec6c0
Patryk Sobow <patryk.sobow95@gmail.com>
2018-10-17 02:24:27 +0200

    commit

--- src/main/java/com/kodilla/rockpaperscissors/game/PlayerMove.java
+++ src/main/java/com/kodilla/rockpaperscissors/game/PlayerMove.java
@@ -6,7 +6,7 @@ class PlayerMove {
     private PlayerMove() {}
 
     static void printPlayerMoves(){
-        printlnMessage(YOUR_TURN_MSG);
+        printMessage(YOUR_TURN_MSG); System.out.println();
         printlnMessageWithPrefix("1.", ROCK_MSG);
         printlnMessageWithPrefix("2.", PAPER_MSG);
         printlnMessageWithPrefix("3.", SCISSORS_MSG);
